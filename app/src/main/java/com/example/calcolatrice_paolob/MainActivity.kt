a49103d6e3e2ad33d97b19a6714ff31e4c78ce0c
Paolo <paolo.benitezpaccha@studenti.unicampania.it>
2019-03-20 16:17:45 +0100

    calcolatrice vuota di paolob

--- app/src/main/java/com/example/calcolatrice_paolob/MainActivity.kt
+++ app/src/main/java/com/example/calcolatrice_paolob/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        a=3
-        b=0
+        
+
     }
 }
